e182f76149e4edef9ed6a2537e3814c20fc4b9d9
Alex Kartel <alex.kartel@gmail.com>
2021-03-17 10:50:58 +0500

    Refactoring

--- calc.py
+++ calc.py
@@ -9,48 +9,55 @@ import tinvest.schemas
 from utils import get_now, localize
 
 TOKEN = os.getenv('TOKEN')
-BROKER_ACCOUNT_STARTED_AT = datetime.strptime('01.08.2020', '%d.%m.%Y')
+BROKER_ACCOUNT_STARTED_AT = datetime.strptime(os.getenv('BROKER_ACCOUNT_START'), '%d.%m.%Y')
+
 FIGI_USD = 'BBG0013HGFT4'
 TICKER_USD = 'USD000UTSTOM'
-
 FIGI_EUR = 'BBG0013HJJ31'
 TICKER_EUR = 'EUR_RUB__TOM'
 
-def get_position_price(client: tinvest.SyncClient, figi: str, day: datetime) -> Decimal:
-    ''' gets price for a day - from [day - 1] to [day] (close candle)''' 
-    return (client.get_market_candles(figi, day - timedelta(days=1), day, tinvest.schemas.CandleResolution('day'))).payload.candles[0].c
+if (TOKEN == None):
+    print('[ERROR] TOKEN variable is empty!')
+    exit()
+if (BROKER_ACCOUNT_STARTED_AT == None):
+    print('[ERROR] BROKER_ACCOUNT_START variable is empty!')
+    exit()
 
 client = tinvest.SyncClient(TOKEN)
 
-oper_list = client.get_operations(BROKER_ACCOUNT_STARTED_AT, datetime.now())
+def get_position_price(figi: str, day: datetime) -> Decimal:
+    ''' gets price for a day - from [day - 1] to [day] (close candle)''' 
+    return (client.get_market_candles(figi, day - timedelta(days=1), day, tinvest.schemas.CandleResolution('day'))).payload.candles[0].c
 
-def get_current_portfolio_price(client: tinvest.SyncClient) -> Decimal:
+def get_current_portfolio_price() -> Decimal:
     sum = 0
     for position in client.get_portfolio().payload.positions:
         candles = client.get_market_candles(position.figi, localize(datetime.now() - timedelta(days=1)), get_now(), tinvest.schemas.CandleResolution('hour')).payload.candles
         value = position.balance * candles[-1].c
         if position.expected_yield.currency == 'USD':
-            value = value * get_position_price(client, FIGI_USD, datetime.now())
+            value = value * get_position_price(FIGI_USD, datetime.now())
         if position.expected_yield.currency == 'EUR':
-            value = value * get_position_price(client, FIGI_EUR, datetime.now())
+            value = value * get_position_price(FIGI_EUR, datetime.now())
         #print(f'{position.name} \t {str(value)}')
         sum += value
     return sum
         
-def get_pays_in(client: tinvest.SyncClient) -> Decimal:
+def get_pays_in() -> Decimal:
     sum = 0
+    oper_list = client.get_operations(BROKER_ACCOUNT_STARTED_AT, datetime.now())
     for operation in oper_list.payload.operations:
         if (operation.operation_type.value == "PayIn") or (operation.operation_type.value == "PayOut"):
             if (operation.currency == 'RUB'):
                 value = operation.payment
             elif (operation.currency == 'USD'):
-                value = operation.payment * get_position_price(client, FIGI_USD, operation.date)
+                value = operation.payment * get_position_price(FIGI_USD, operation.date)
             elif (operation.currency == 'EUR'):
-                value = operation.payment * get_position_price(client, FIGI_EUR, operation.date)
+                value = operation.payment * get_position_price(FIGI_EUR, operation.date)
             sum += value
     return sum
 
-#print(f'{operation.operation_type.value}:\t{str(operation.payment)} {operation.currency}\t{value}\t{operation.date.date()}')
 
-print('=====================')
-print(f'Overall income:\t{str(get_current_portfolio_price(client) - get_pays_in(client))}')
+if ( __name__ == "__main__" ):
+    overall = round(get_current_portfolio_price() - get_pays_in(), 2)
+    print('=====================')
+    print(f'Overall income:\t{str(overall)} RUB')
